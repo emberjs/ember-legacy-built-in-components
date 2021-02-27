5f2949e343a2b6cbe48f886005886138c45ce95a
jame mackson <jamemackson@icloud.com>
2021-02-27 03:17:15 -0600

    WIP: move link-to, text-field, textarea, checkbox

---
+++ tests/unit/mixins/text-support-test.ts
@@ -0,0 +1,12 @@
+import EmberObject from '@ember/object';
+import TextSupportMixin from 'ember-legacy-built-in-components/mixins/text-support';
+import { module, test } from 'qunit';
+
+module('Unit | Mixin | TextSupport', function() {
+  // Replace this with your real tests.
+  test('it works', function (assert) {
+    let TextSupportObject = EmberObject.extend(TextSupportMixin);
+    let subject = TextSupportObject.create();
+    assert.ok(subject);
+  });
+});
